2aaa93f4321a31be8f2e22de595f5a61a071b0a7
Sriragbadveti <badvetisrirag@gmail.com>
2025-06-15 00:42:22 +0400

    testo

--- routes/abstractRouter.js
+++ routes/abstractRouter.js
@@ -1,26 +1,30 @@
 // routes/abstractRoutes.js
 const express = require("express");
 const multer = require("multer");
-const path = require("path")
+const path = require("path");
 const Abstract = require("../models/abstractModel.js");
 
-const router = express.Router()
-
+const router = express.Router();
+const fs = require("fs");
+const uploadsDir = path.join(__dirname, "../uploads/abstracts");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 // File upload setup using multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/abstracts/")
+    cb(null, "uploads/abstracts/");
   },
   filename: function (req, file, cb) {
-    const ext = path.extname(file.originalname)
-    cb(null, `${Date.now()}-${file.fieldname}${ext}`)
+    const ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}-${file.fieldname}${ext}`);
   },
-})
+});
 
 const upload = multer({
   storage,
   limits: { fileSize: 10 * 1024 * 1024 }, // Max 10MB
-})
+});
 
 // @route   POST /api/abstracts
 // @desc    Submit abstract form
@@ -40,10 +44,10 @@ router.post("/submission", upload.single("abstractFile"), async (req, res) => {
       originalityConsent,
       disqualificationConsent,
       permissionConsent,
-    } = req.body
+    } = req.body;
 
     if (!req.file) {
-      return res.status(400).json({ error: "File is required" })
+      return res.status(400).json({ error: "File is required" });
     }
 
     const newAbstract = new Abstract({
@@ -61,14 +65,14 @@ router.post("/submission", upload.single("abstractFile"), async (req, res) => {
       originalityConsent,
       disqualificationConsent,
       permissionConsent,
-    })
+    });
 
-    await newAbstract.save()
-    res.status(201).json({ message: "Abstract submitted successfully" })
+    await newAbstract.save();
+    res.status(201).json({ message: "Abstract submitted successfully" });
   } catch (err) {
-    console.error("Abstract submission failed", err)
-    res.status(500).json({ error: "Internal server error" })
+    console.error("Abstract submission failed", err);
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 
 module.exports = router;
