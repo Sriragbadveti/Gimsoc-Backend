b81378193d7ed5970c809f90a0fff29bb4c502d7
Sriragbadveti <badvetisrirag@gmail.com>
2025-07-29 23:08:53 +0530

    dd

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -143,6 +143,7 @@ router.get("/getalltickets", adminAuthMiddleware, async (req, res) => {
       foodPreference: ticket.foodPreference,
       dietaryRestrictions: ticket.dietaryRestrictions,
       accessibilityNeeds: ticket.accessibilityNeeds,
+      galaDinner: ticket.galaDinner,
       isTsuStudent: ticket.isTsuStudent,
       tsuEmail: ticket.tsuEmail,
       isGimsocMember: ticket.isGimsocMember,
